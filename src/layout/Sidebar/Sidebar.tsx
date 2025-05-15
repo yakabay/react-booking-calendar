018f5687f8daa4cb68a4192d8f2858fbed56b4c5
yakabay <yakabay86@gmail.com>
2025-05-15 19:42:28 +0200

    Refactor box-shadow styles in BookingCalendar, ClientForm, and PractitionerPage for consistency; add active state styling to Sidebar links.

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,18 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import clsx from "clsx";
 import "./Sidebar.scss";
 
 export const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <aside className="sidebar" role="complementary">
       <nav className="sidebar__nav" role="navigation">
         <ul className="sidebar__nav-list">
           <li className="sidebar__nav-item">
-            <Link to="/client" className="sidebar__nav-link">
+            <Link
+              to="/client"
+              className={clsx("sidebar__nav-link", {
+                "sidebar__nav-link--active": location.pathname === "/client",
+              })}
+            >
               Client
             </Link>
           </li>
           <li className="sidebar__nav-item">
-            <Link to="/practitioner" className="sidebar__nav-link">
+            <Link
+              to="/practitioner"
+              className={clsx("sidebar__nav-link", {
+                "sidebar__nav-link--active": location.pathname === "/practitioner",
+              })}
+            >
               Practitioner
             </Link>
           </li>
