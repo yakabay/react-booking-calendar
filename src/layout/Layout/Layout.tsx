10d5528440c854e2b5b8c73820afd13937cb0c57
yakabay <yakabay86@gmail.com>
2025-05-16 14:51:46 +0200

    Implement scroll-to-top functionality in Layout component and adjust media queries in PractitionerPage styles for better responsiveness

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Sidebar } from "../Sidebar/Sidebar";
 import "./Layout.scss";
+import { useEffect, useRef } from "react";
 
 export const Layout = () => {
+  const location = useLocation();
+  const mainRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
+
   return (
     <div className="layout">
       <div className="layout__sidebar">
         <Sidebar />
       </div>
-      <main className="layout__main">
+      <main ref={mainRef} className="layout__main">
         <Outlet />
       </main>
     </div>
